34a5a8b2f5f4bbc25afb7b62f6dc32abddc06632
KelTamNguyen <keltamnguyen@yahoo.com>
2024-12-10 19:08:20 -0600

    Refactor Javascript code according to The Odin Prject's curriculum
    All "var" variables that can change have been changed to "let" instead.
    
    The choices array has been changed to const and its name has been
    capitalized, as suggested by material from The Odin Project.
    
    Variable names have been changed to better follow the Rock Paper
    Scissors exercise in The Odin Project. Variables with "selection" have
    been changed to "choice" and "player" has been changed to "human". The
    function computerPlay() has been renamed getComputerChoice().

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
 // Game Variables
-var choices = ['rock', 'paper', 'scissors'];
-var playerScore = 0;
-var computerScore = 0;
-var round = 1;
-var drawCount = 0;
+const CHOICES = ['rock', 'paper', 'scissors'];
+let humanScore = 0;
+let computerScore = 0
+let round = 1;
+let drawCount = 0;
 
 // DOM/UI
-var rock = document.querySelector('#rock');
-var paper = document.querySelector('#paper');
-var scissors = document.querySelector('#scissors');
-var p1Score = document.querySelector('#p1');
-var cpuScore = document.querySelector('#cpu');
-var roundNumber = document.querySelector('#round');
-var result = document.querySelector('#result');
-var modalBg = document.querySelector('.modal-bg');
-var modalMessage = document.querySelector('.modal h2');
-var playAgain = document.querySelector('.play-again')
-var modalClose = document.querySelector('.modal-close');
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+let p1Score = document.querySelector('#p1');
+let cpuScore = document.querySelector('#cpu');
+let roundNumber = document.querySelector('#round');
+let result = document.querySelector('#result');
+let modalBg = document.querySelector('.modal-bg');
+let modalMessage = document.querySelector('.modal h2');
+let playAgain = document.querySelector('.play-again')
+let modalClose = document.querySelector('.modal-close');
 
 rock.addEventListener('click', () => handleClick('rock'));
 paper.addEventListener('click', () => handleClick('paper'));
 scissors.addEventListener('click', () => handleClick('scissors'));
 playAgain.addEventListener('click', () => resetGame());
-modalClose.addEventListener('click', function() {
-    modalBg.classList.remove('bg-active');
-});
+modalClose.addEventListener('click', () => modalBg.classList.remove('bg-active'));
 
-function handleClick(value) {
-    let computerSelection = computerPlay();
-    playRound(value, computerSelection);
-}
-
-
-function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)];
+function getComputerChoice() {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
 function capitalizeFirstLetter(string) {
@@ -45,6 +37,43 @@ function openModal(message) {
     modalBg.classList.add('bg-active');
 }
 
+function handleClick(value) {
+    let computerChoice = getComputerChoice();
+    playRound(value, computerChoice);
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanScore !== 5 && computerScore !== 5) {
+        round++;
+        roundNumber.textContent = round;
+        if (humanChoice === 'rock' && computerChoice === 'scissors' || 
+            humanChoice === 'scissors' && computerChoice=== 'paper' || 
+            humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore++;
+            p1Score.textContent = humanScore;
+            result.textContent = `You Win! ${capitalizeFirstLetter(humanChoice)} beats ${computerChoice}`;
+            if (humanScore === 5) {
+                openModal('You Win!');
+            }
+        } 
+        else if (humanChoice === 'rock' && computerChoice === 'paper' || 
+                humanChoice === 'paper' && computerChoice === 'scissors' || 
+                humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore++;
+            cpuScore.textContent = computerScore;
+            result.textContent = `You Lose! ${capitalizeFirstLetter(computerChoice)} beats ${humanChoice}`;
+            if (computerScore === 5) {
+                openModal('Game Over');
+            }
+        }
+        else if (humanChoice === computerChoice) {
+            drawCount++;
+            result.textContent = `Draw (x${drawCount})!`;
+        }
+    }
+    // console.log(result.textContent);
+}
+
 function resetGame() {
     playerScore = 0;
     computerScore = 0;
@@ -54,31 +83,4 @@ function resetGame() {
     cpuScore.textContent = computerScore;
     roundNumber.textContent = round;
     modalBg.classList.remove('bg-active');
-}
-
-function playRound(playerSelection, computerSelection) {
-    if (playerScore !== 5 && computerScore !== 5) {
-        round++;
-        roundNumber.textContent = round;
-        if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'rock') {
-            playerScore++;
-            p1Score.textContent = playerScore;
-            result.textContent = `You Win! ${capitalizeFirstLetter(playerSelection)} beats ${computerSelection}`;
-            if (playerScore === 5) {
-                openModal('You Win!');
-            }
-        } 
-        else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
-            computerScore++;
-            cpuScore.textContent = computerScore;
-            result.textContent = `You Lose! ${capitalizeFirstLetter(computerSelection)} beats ${playerSelection}`;
-            if (computerScore === 5) {
-                openModal('Game Over');
-            }
-        }
-        else if (playerSelection === computerSelection) {
-            drawCount++;
-            result.textContent = `Draw (x${drawCount})!`;
-        }
-    }
 }
